0eacdcefa8c72a371ab1f3e59c9818b83663aaa7
islam dya <49913188+dukeiso199@users.noreply.github.com>
2020-01-15 02:16:21 +0200

    recent development

---
+++ trial for board game/Assets/gamecontrol.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gamecontrol : MonoBehaviour
+{
+    public int nooftiles = 6;
+    public static gamecontrol instance;
+    public GameObject player;
+    public Transform[] tiles;
+    public Text dicenumber;
+    public int roll = 0;
+
+    // singlton
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+    else if ( instance != this)
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+
+    private void FixedUpdate()
+    {
+        dicenumber.text = "" + roll;
+    }
+    void Update()
+    {
+        
+    }
+    public int diceroll ()
+    {
+        roll = Random.Range(1, 6);
+        return roll;
+    }
+}
---
+++ trial for board game/Assets/player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public int currentposition = 0;
+    void Start()
+    {
+        transform.position = gamecontrol.instance.tiles[currentposition].transform.position;
+    }
+    public void movebydice ()
+    {
+        int numberofmoves = gamecontrol.instance.roll;
+        for (int i = numberofmoves; i>0; i --)
+        {
+            if (currentposition > 6)
+            {
+                currentposition = 0;
+            }
+            movestep();
+        }
+    }
+    public void movestep ()
+    {
+        transform.position = gamecontrol.instance.tiles[currentposition + 1].transform.position;
+        currentposition++;
+    }
+    void Update()
+    {
+        
+    }
+}
